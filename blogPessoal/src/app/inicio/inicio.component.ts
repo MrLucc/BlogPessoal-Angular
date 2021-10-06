d165dcf151449ddfc3240b71ae1c616f02576862
Lucas <silvalucs212@gmail.com>
2021-10-06 20:16:52 -0300

    Implementação do post e get de postagens

--- blogPessoal/src/app/inicio/inicio.component.ts
+++ blogPessoal/src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +15,21 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagem: Postagem[]
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+  
+  user: User = new User
+  idUser = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(){
@@ -18,6 +37,49 @@ export class InicioComponent implements OnInit {
     if(environment.token == ""){
       this.router.navigate(["/entrar"])
     }
+
+    this.getAllTemas()
+    this.getAllPostagens()
+
+  }
+
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagem = resp
+    })
+  }
+
+  findByIdUser(){
+    this.authService.getByIdUser(this.idUser).subscribe((resp: User)=>{
+      this.user = resp
+    })
+  }
+  
+  publicar(){
+    this.tema.idTema = this.idTema
+    this.postagem.temaRelacionado = this.tema
+
+    this.user.idUsuario = this.idUser
+    this.postagem.criador = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert("Postagem realizada com sucesso!")
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
   }
 
 }
--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -23,6 +23,10 @@ export class AuthService {
 
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogmrlucc.herokuapp.com/usuario/${id}`)
+  }
+
   logado(){
     let ok: boolean = false
 
